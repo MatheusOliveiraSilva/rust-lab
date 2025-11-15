d03fb013166dae5f5bf3351a5ad603e76c5ab997
Matheus Oliveira Silva <matheussilva@tecgraf.puc-rio.br>
2025-11-15 11:22:03 -0300

    more borrowing test

--- fundamentals/tests/borrowing_tests.rs
+++ fundamentals/tests/borrowing_tests.rs
@@ -7,3 +7,26 @@ fn test_min_max() {
     assert_eq!(*min, 1);
     assert_eq!(*max, 10);
 }
+
+#[test]
+fn test_negative_inputs_min_max() {
+    let v = vec![-1, -10, -2, -5];
+    let (min, max) = min_max(&v).unwrap();
+    assert_eq!(*min, -10);
+    assert_eq!(*max, -1);
+}
+
+#[test]
+fn test_one_element_min_max() {
+    let v = vec![1];
+    let (min, max) = min_max(&v).unwrap();
+    assert_eq!(*min, 1);
+    assert_eq!(*max, 1);
+}
+
+#[test]
+fn test_empty_min_max() {
+    let v = vec![];
+    let result = min_max(&v);
+    assert_eq!(result, None);
+}
