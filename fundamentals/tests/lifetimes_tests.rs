8bcf3ed520adf7135b0baf2db350c7b19ef5ab8e
Matheus Oliveira Silva <matheussilva@tecgraf.puc-rio.br>
2025-11-15 14:00:13 -0300

    Add tests for the select_range function

--- fundamentals/tests/lifetimes_tests.rs
+++ fundamentals/tests/lifetimes_tests.rs
@@ -1,4 +1,4 @@
-use fundamentals::lifetimes::{take_slice};
+use fundamentals::lifetimes::{take_slice, select_range};
 
 #[test]
 fn test_basic_take_slice() {
@@ -29,5 +29,37 @@ fn test_out_of_index_take_slice() {
     let s = "Hello word!";
     let (start, end) = (5, 25); // end > s.len() -> None
     let result = take_slice(s, start, end);
+    assert_eq!(result, None);
+}
+
+#[test]
+fn test_select_range() {
+    let buffer = "Hello word!";
+    let (start, end) = (0, 5);
+    let selection = select_range(buffer, start, end).unwrap();
+    assert_eq!(selection, "Hello");
+
+    let (start, end) = (5, 0);
+    let selection = select_range(buffer, start, end).unwrap();
+    assert_eq!(selection, "Hello");
+
+    let (start, end) = (2, 5);
+    let selection = select_range(buffer, start, end).unwrap();
+    assert_eq!(selection, "llo");
+
+    let (start, end) = (5, 2);
+    let selection = select_range(buffer, start, end).unwrap();
+    assert_eq!(selection, "llo");
+
+    let (start, end) = (2, 2);
+    let result = select_range(buffer, start, end);
+    assert_eq!(result, None);
+
+    let (start, end) = (2, 25);
+    let result = select_range(buffer, start, end);
+    assert_eq!(result, None);
+
+    let (start, end) = (25, 2);
+    let result = select_range(buffer, start, end);
     assert_eq!(result, None);
 }
